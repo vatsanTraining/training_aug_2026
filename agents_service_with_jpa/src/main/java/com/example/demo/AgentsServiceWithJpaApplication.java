195faa5d047387160b18b1f7af884e516126aaeb
srivatsan <vatsan_k@hotmail.com>
2026-08-19 15:13:35 +0530

    Update

--- agents_service_with_jpa/src/main/java/com/example/demo/AgentsServiceWithJpaApplication.java
+++ agents_service_with_jpa/src/main/java/com/example/demo/AgentsServiceWithJpaApplication.java
@@ -1,7 +1,15 @@
 package com.example.demo;
 
+import java.time.LocalDate;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.demo.entity.TravelAgent;
+import com.example.demo.enums.AgentRole;
+import com.example.demo.services.TravelAgentService;
 
 @SpringBootApplication
 public class AgentsServiceWithJpaApplication {
@@ -10,4 +18,11 @@ public class AgentsServiceWithJpaApplication {
 		SpringApplication.run(AgentsServiceWithJpaApplication.class, args);
 	}
 
+	
+	@Bean
+	CommandLineRunner runner(TravelAgentService service) {
+		
+		return args -> service.save(new TravelAgent("Ramesh", 8999494, LocalDate.now(), AgentRole.MANAGER));
+		
+	}
 }
